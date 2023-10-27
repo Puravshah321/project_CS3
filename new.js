f943757e39c427e2dfde79e678b967000d6e6dc2
Puravshah321 <124144406+Puravshah321@users.noreply.github.com>
2023-10-27 16:46:42 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+
+		let popup= document.getElementById("popup");
+		
+		function opencard(){
+		  popup.classList.add("open-popup");
+		}
+  
+		function closecard(){
+		  popup.classList.remove("open-popup");
+		}
